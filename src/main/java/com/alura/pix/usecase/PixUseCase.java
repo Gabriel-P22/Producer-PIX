531fb192c1b1e92a5da532720dfb2c64f9b52f70
Gabriel <paschoalsoftwareengineer@gmail.com>
2024-10-02 15:15:23 -0300

    Create kafka consfig and docker-compose config

--- src/main/java/com/alura/pix/usecase/PixUseCase.java
+++ src/main/java/com/alura/pix/usecase/PixUseCase.java
@@ -5,6 +5,7 @@ import com.alura.pix.infrastructure.database.model.Pix;
 import com.alura.pix.infrastructure.database.repository.PixRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -13,8 +14,11 @@ public class PixUseCase {
 
   @Autowired private final PixRepository repository;
 
+  @Autowired private final KafkaTemplate<String, PixDto> kafkaTemplate;
+
   public PixDto savePix(PixDto pixDto) {
     repository.save(Pix.toEntity(pixDto));
+    kafkaTemplate.send("pix-topic", pixDto.getIdentifier(), pixDto);
     return pixDto;
   }
 }
